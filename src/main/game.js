6209d3764c309ca358e88f76d764f5eced8eb3a1
Antares <ericwillner@proton.me>
2025-04-02 15:45:55 +0200

    Fixed a little Placements and wiring

--- src/main/game.js
+++ src/main/game.js
@@ -9,12 +9,33 @@ var console_1 = require("console");
 var moneyManagment_1 = require("../entities/moneyManagment");
 var Game = /** @class */ (function () {
     function Game() {
+        this.entities = [];
         this.FCIDCOUNTER = 0;
         this.IPIDCOUNTER = 0;
+        this.placementCooldown = 500;
+        this.lastPlacementTime = 0;
         this.coinManager = new moneyManagment_1.CoinManager();
+        this.secondConnectingLevel = false;
+        this.equipIndex = 1;
+        this.targetPoint1 = null;
+        this.targetPoint2 = null;
         this.r = raylib;
-        //this.placeFactory()
+        this.placementCooldown = 500; // 500ms cooldown
+        this.lastPlacementTime = 0;
     }
+    Game.prototype.isPositionOccupied = function (x, y, width, height) {
+        for (var _i = 0, _a = this.entities; _i < _a.length; _i++) {
+            var entity = _a[_i];
+            var ex = entity.position[0];
+            var ey = entity.position[1];
+            var ew = entity instanceof factory_1.default ? entity.size[0] : 10;
+            var eh = entity instanceof factory_1.default ? entity.size[1] : 10;
+            if (x < ex + ew && x + width > ex && y < ey + eh && y + height > ey) {
+                return true;
+            }
+        }
+        return false;
+    };
     Game.prototype.createInputPoint = function (x, y, wires) {
         var targetFactory = null;
         for (var _i = 0, _a = this.entities; _i < _a.length; _i++) {
@@ -25,6 +46,7 @@ var Game = /** @class */ (function () {
                 var fw = entity.size[0];
                 var fh = entity.size[1];
                 if (x >= fx && x <= fx + fw && y >= fy && y <= fy + fh) {
+                    (0, console_1.log)("Checking Factory ".concat(entity.id, " at (").concat(fx, ", ").concat(fy, ") with size ").concat(fw, "x").concat(fh));
                     targetFactory = entity;
                     break;
                 }
@@ -36,19 +58,35 @@ var Game = /** @class */ (function () {
         }
     };
     Game.prototype.placeInputPoint = function () {
-        this.r.DrawRectangle(this.r.GetMouseX(), this.r.GetMouseY(), 10, 10, this.r.RED);
-        if (this.r.IsMouseButtonDown(this.r.MOUSE_BUTTON_RIGHT)) {
-            this.createInputPoint(this.r.GetMouseX(), raylib.GetMouseY(), []);
-            (0, console_1.log)("added IP: " + this.IPIDCOUNTER);
+        if (Date.now() - this.lastPlacementTime < this.placementCooldown)
+            return;
+        if (this.r.IsMouseButtonDown(this.r.MOUSE_BUTTON_RIGHT) && this.equipIndex === 2) {
+            this.createInputPoint(this.r.GetMouseX(), this.r.GetMouseY(), []);
+            (0, console_1.log)("added IP: " + this.IPIDCOUNTER + this.entities);
+            this.lastPlacementTime = Date.now();
         }
     };
     Game.prototype.placeFactory = function () {
-        this.r.DrawRectangle(this.r.GetMouseX(), this.r.GetMouseY(), 50, 30, this.r.BLUE);
-        if (this.r.IsMouseButtonDown(this.r.MOUSE_BUTTON_LEFT)) {
+        if (Date.now() - this.lastPlacementTime < this.placementCooldown)
+            return;
+        var mouseX = this.r.GetMouseX();
+        var mouseY = this.r.GetMouseY();
+        if (this.isPositionOccupied(mouseX, mouseY, 50, 30))
+            return;
+        if (this.r.IsMouseButtonDown(this.r.MOUSE_BUTTON_RIGHT) && this.equipIndex === 1) {
             this.FCIDCOUNTER++;
-            this.entities.push(new factory_1.default("FC" + this.FCIDCOUNTER, [this.r.GetMouseX(), raylib.GetMouseY()], [50, 30]));
+            this.entities.push(new factory_1.default("FC" + this.FCIDCOUNTER, [mouseX, mouseY], [50, 30]));
             (0, console_1.log)("added FC: " + this.FCIDCOUNTER);
+            this.lastPlacementTime = Date.now();
         }
+    };
+    Game.prototype.checkInput = function () {
+        for (var i = 0; i <= 9; i++) {
+            if (this.r.IsKeyDown(this.r.KEY_ZERO + i)) {
+                this.equipIndex = i;
+            }
+        }
+        this.r.DrawText(String(this.equipIndex), 10, 10, 10, this.r.BLACK);
     };
     Game.prototype.draw = function () {
         var screenWidth = 1920;
@@ -60,6 +98,7 @@ var Game = /** @class */ (function () {
             this.r.ClearBackground(this.r.RAYWHITE);
             this.placeFactory();
             this.placeInputPoint();
+            this.checkInput();
             for (var _i = 0, _a = this.entities; _i < _a.length; _i++) {
                 var entity = _a[_i];
                 if (entity instanceof wire_1.default) {
@@ -72,7 +111,6 @@ var Game = /** @class */ (function () {
                     this.r.DrawRectangle(entity.position[0], entity.position[1], 10, 10, this.r.RED);
                 }
             }
-            this.entities = this.entities;
             this.r.EndDrawing();
         }
         this.r.CloseWindow();
--- src/main/game.ts
+++ src/main/game.ts
@@ -3,16 +3,37 @@ import Entity from '../entities/entity';
 import Wire from '../entities/wire';
 import Factory from '../entities/factory';
 import InputPoint from '../entities/inputPoint';
+import OutputPoint from '../entities/outputPoint';
 import { log } from 'console';
 import { CoinManager } from '../entities/moneyManagment';
 
 export class Game {
     r: typeof raylib;
-    entities: Entity[]
-    FCIDCOUNTER: number = 0
-    IPIDCOUNTER: number = 0
+    entities: Entity[] = [];
+    FCIDCOUNTER: number = 0;
+    IPIDCOUNTER: number = 0;
+    placementCooldown: number = 500;
+    lastPlacementTime: number = 0;
 
     coinManager: CoinManager = new CoinManager();
+    secondConnectingLevel: boolean = false;
+    equipIndex: number = 1;
+    targetPoint1: Entity | null = null;
+    targetPoint2: Entity | null = null;
+
+    isPositionOccupied(x: number, y: number, width: number, height: number): boolean {
+        for (const entity of this.entities) {
+            const ex = entity.position[0];
+            const ey = entity.position[1];
+            const ew = entity instanceof Factory ? entity.size[0] : 10;
+            const eh = entity instanceof Factory ? entity.size[1] : 10;
+            
+            if (x < ex + ew && x + width > ex && y < ey + eh && y + height > ey) {
+                return true;
+            }
+        }
+        return false;
+    }
 
     createInputPoint(x: number, y: number, wires: Wire[]) {
         let targetFactory: Factory | null = null;
@@ -22,8 +43,8 @@ export class Game {
                 const fy = entity.position[1];
                 const fw = entity.size[0];
                 const fh = entity.size[1];
-                
                 if (x >= fx && x <= fx + fw && y >= fy && y <= fy + fh) {
+                    log(`Checking Factory ${entity.id} at (${fx}, ${fy}) with size ${fw}x${fh}`);
                     targetFactory = entity;
                     break;
                 }
@@ -33,57 +54,75 @@ export class Game {
         if (targetFactory) {
             this.IPIDCOUNTER++;
             this.entities.push(new InputPoint("IP" + this.IPIDCOUNTER, [x, y], wires, targetFactory));
-        }
+        }        
     }
 
     placeInputPoint() {
-        this.r.DrawRectangle(this.r.GetMouseX(), this.r.GetMouseY(), 10, 10, this.r.RED)
-        if(this.r.IsMouseButtonDown(this.r.MOUSE_BUTTON_RIGHT)) {
-            this.createInputPoint(this.r.GetMouseX(), raylib.GetMouseY(), [])
-            log("added IP: " + this.IPIDCOUNTER)
+        if (Date.now() - this.lastPlacementTime < this.placementCooldown) return;
+        if (this.r.IsMouseButtonDown(this.r.MOUSE_BUTTON_RIGHT) && this.equipIndex === 2) {
+            this.createInputPoint(this.r.GetMouseX(), this.r.GetMouseY(), []);
+            log("added IP: " + this.IPIDCOUNTER + this.entities);
+            this.lastPlacementTime = Date.now();
         }
     }
 
     placeFactory() {
-        this.r.DrawRectangle(this.r.GetMouseX(), this.r.GetMouseY(), 50, 30, this.r.BLUE)
-        if(this.r.IsMouseButtonDown(this.r.MOUSE_BUTTON_LEFT)) {
-            this.FCIDCOUNTER++
-            this.entities.push(new Factory("FC" + this.FCIDCOUNTER, [this.r.GetMouseX(), raylib.GetMouseY()], [50, 30]))
-            log("added FC: " + this.FCIDCOUNTER)
+        if (Date.now() - this.lastPlacementTime < this.placementCooldown) return;
+        const mouseX = this.r.GetMouseX();
+        const mouseY = this.r.GetMouseY();
+        
+        if (this.isPositionOccupied(mouseX, mouseY, 50, 30)) return;
+        
+        if (this.r.IsMouseButtonDown(this.r.MOUSE_BUTTON_RIGHT) && this.equipIndex === 1) {
+            this.FCIDCOUNTER++;
+            this.entities.push(new Factory("FC" + this.FCIDCOUNTER, [mouseX, mouseY], [50, 30]));
+            log("added FC: " + this.FCIDCOUNTER);
+            this.lastPlacementTime = Date.now();
         }
-
     }
+
+    checkInput() {
+        for (let i: number = 0; i <= 9; i++) {
+            if (this.r.IsKeyDown(this.r.KEY_ZERO + i)) {
+                this.equipIndex = i;
+            }
+        } 
+        this.r.DrawText(String(this.equipIndex), 10, 10, 10, this.r.BLACK);
+    }
+
     draw() {
-        const screenWidth = 1920
-        const screenHeight = 1028
-        this.r.InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window")
-        this.r.SetTargetFPS(128)
+        const screenWidth = 1920;
+        const screenHeight = 1028;
+        this.r.InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+        this.r.SetTargetFPS(128);
 
         while (!this.r.WindowShouldClose()) {
             this.r.BeginDrawing();
-            this.r.ClearBackground(this.r.RAYWHITE)
-            this.placeFactory()
-            this.placeInputPoint()
+            this.r.ClearBackground(this.r.RAYWHITE);
+            this.placeFactory();
+            this.placeInputPoint();
+            this.checkInput();
             for (const entity of this.entities) {
                 if (entity instanceof Wire) {
-                    this.r.DrawLine(entity.position[0], entity.position[1], entity.endPosition[0], entity.endPosition[1], this.r.BLUE)
+                    this.r.DrawLine(entity.position[0], entity.position[1], entity.endPosition[0], entity.endPosition[1], this.r.BLUE);
                 } else if (entity instanceof Factory) {
-                    this.r.DrawRectangle(entity.position[0], entity.position[1], entity.size[0], entity.size[1], this.r.DARKBLUE)
+                    this.r.DrawRectangle(entity.position[0], entity.position[1], entity.size[0], entity.size[1], this.r.DARKBLUE);
                 } else if (entity instanceof InputPoint) {
-                    this.r.DrawRectangle(entity.position[0], entity.position[1], 10, 10, this.r.RED)
+                    this.r.DrawRectangle(entity.position[0], entity.position[1], 10, 10, this.r.RED);
                 }
             }
-            this.entities = this.entities;
-            this.r.EndDrawing()
+            this.r.EndDrawing();
         }
-        this.r.CloseWindow()
+        this.r.CloseWindow();
     }
+
     returnR(): typeof raylib {
         return this.r;
     }
 
     constructor() {
         this.r = raylib;
-        //this.placeFactory()
+        this.placementCooldown = 500; // 500ms cooldown
+        this.lastPlacementTime = 0;
     }
 }
